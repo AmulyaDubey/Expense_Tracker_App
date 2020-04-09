80444687657a45f16625310ac4e2bbe7d98daae4
Amulya <macbookpro@Amulyas-MacBook-Pro.local>
2020-04-09 23:09:54 +0530

    Recycler View Progress

--- app/src/main/java/com/example/expense/UserAdapter.java
+++ app/src/main/java/com/example/expense/UserAdapter.java
@@ -4,38 +4,54 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class UserAdapter extends ArrayAdapter<Expenditure>
-{
-    public UserAdapter(Context context, ArrayList<Expenditure> users)
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Expenditure> profiles;
+
+    public UserAdapter(Context c , ArrayList<Expenditure> p)
     {
-        super(context, 0, users);
+        context = c;
+        profiles = p;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.items_list, parent, false));
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
-        // Get the data item for this position
-        Expenditure obj = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_view, parent, false);
-        }
-        // Lookup view for data population
-        TextView mOption = (TextView) convertView.findViewById(R.id.mOption);
-        TextView mAmt = (TextView) convertView.findViewById(R.id.mAmt);
-        TextView mDate=(TextView)convertView.findViewById(R.id.mDate);
-        // Populate the data into the template view using the data object
-        mOption.setText(obj.getOption());
-        mAmt.setText(obj.getAmt());
-        mDate.setText(obj.getDate());
-        // Return the completed view to render on screen
-        return convertView;
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        holder.mOption.setText(profiles.get(position).getOption());
+        holder.mDate.setText(profiles.get(position).getDate());
+        holder.mAmt.setText(profiles.get(position).getAmt());
     }
+
+    @Override
+    public int getItemCount() {
+        return profiles.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView mOption, mDate, mAmt;
+        RecyclerView parentlayout;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            mOption = itemView.findViewById(R.id.mOption);
+            mAmt = itemView.findViewById(R.id.mAmt);
+            mDate = itemView.findViewById(R.id.mDate);
+            parentlayout = itemView.findViewById(R.id.recyclerView);
+        }
+    }
+
+
 }
-
-
--- app/src/main/java/com/example/expense/ViewActivity.java
+++ app/src/main/java/com/example/expense/ViewActivity.java
@@ -1,13 +1,12 @@
 package com.example.expense;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,41 +19,35 @@ import java.util.ArrayList;
 
 public class ViewActivity extends AppCompatActivity {
 
+    private DatabaseReference db;
     private FirebaseAuth mAuth;
-    private EditText edit;
-    private FirebaseDatabase db;
-    private ListView listView;
     private ArrayList<Expenditure> list;
-    private ArrayAdapter<Expenditure> adapter;
-    private DatabaseReference reference;
+    private UserAdapter adapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
 
-        //edit=(EditText)findViewById(R.id.edit);
         mAuth = FirebaseAuth.getInstance();
-
-        UserAdapter adapter = new UserAdapter(this, list);
-        listView.setAdapter(adapter);
-
-        //list.add("Hello");
         String user_id=mAuth.getCurrentUser().getUid();
-        reference=FirebaseDatabase.getInstance().getReference("users").child(user_id).child("Addition").child("Regular");
-        reference.addValueEventListener(new ValueEventListener() {
+
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db=FirebaseDatabase.getInstance().getReference("users").child(user_id).child("Addition").child("Self Salary");
+        db.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list = new ArrayList<>();
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     Expenditure obj=snapshot.getValue(Expenditure.class);
-                    Toast.makeText(ViewActivity.this,""+obj.getAmt()+" "+obj.getOption()+" "+obj.getDes(),Toast.LENGTH_LONG).show();
-                    //String txt="";
-                    //txt= obj.getDes() +" "+ obj.getAmt() +" "+ obj.getOption();
-                    // list.add(txt);
                     list.add(obj);
                 }
+                adapter = new UserAdapter(ViewActivity.this,list);
+                recyclerView.setAdapter(adapter);
 
-                //adapter.notifyDataSetChanged();
             }
 
             @Override
@@ -64,6 +57,6 @@ public class ViewActivity extends AppCompatActivity {
         });
     }
 
-
 }
+
 
